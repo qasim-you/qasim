68ec733785af468c0b1ddf0e815788db991cdb82
Muhammad Qasim <123858146+qasim-you@users.noreply.github.com>
2025-07-17 22:51:26 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -183,7 +183,7 @@ export default function Home() {
               </AnimatedSection>
             </div>
           </div>
-
+  
           {/* Footer */}
           <AnimatedSection
             animation="fade-in"
